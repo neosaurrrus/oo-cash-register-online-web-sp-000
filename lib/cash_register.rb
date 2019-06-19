c690db1f6058cf33f00d29285015043ad5b17fb8
Lukie Kang <lukiek+f@gmail.com>
2019-06-19 22:00:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,20 +10,20 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += price*quantity
+    @last_transaction = price * quantity
     quantity.times do
       @items << title
     end
   end # of add_item
 
   def apply_discount
-
     total_discount = (@total/100)*@discount
     puts "total is #{@total} so total_discount is #{total_discount}"
     @total -= total_discount
     @discount == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{@total}."
   end # of apply_discount
 
-
+  
 
 
 end #of class
