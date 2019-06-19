e0612adee9380c4be7fe9967ae451eb0c6833940
Lukie Kang <lukiek+f@gmail.com>
2019-06-19 21:46:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
     @discount = discount
   end #of initialise
 
-  def add_item(title, price)
+  def add_item(title, price, quantity = 1)
     @total += price
   end # of add_item
 end #of class
