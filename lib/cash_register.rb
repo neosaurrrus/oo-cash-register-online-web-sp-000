6a8df8f3a7ecf1e021b177bc0f581e384d632608
Lukie Kang <lukiek+f@gmail.com>
2019-06-19 21:58:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += price*quantity
-    quantity.loop do
+    quantity.do
       @items << title
     end
   end # of add_item
