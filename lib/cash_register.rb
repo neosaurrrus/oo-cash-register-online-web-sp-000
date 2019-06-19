996b52b9d8d8aa616e5543e895c0582e9b210c03
Lukie Kang <lukiek+f@gmail.com>
2019-06-19 22:01:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 
 class CashRegister
-  attr_accessor :total, :discount, :previous_total, :items
+  attr_accessor :total, :discount, :last_transaction, :items
 
   def initialize(discount = 0)
     @total = 0
@@ -23,7 +23,9 @@ class CashRegister
     @discount == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{@total}."
   end # of apply_discount
 
-
+  def void_last_transaction
+    @total += @last_transaction
+  end
 
 
 end #of class
